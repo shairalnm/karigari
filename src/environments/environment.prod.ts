2733e10a7aeb122e8daf77974356101e5b2f7f72
shairalnm <shairalnm@gmail.com>
2020-08-12 13:17:58 -0400

    new carousel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyB2n9wRI4Q3PzvySUNBiAu9w0ZpCHTWzwg',
+  firebaseJewelryDatabaseLink:
+    'https://karigari-90b30.firebaseio.com/jewelry.json',
+  firebaseContactDatabaseLink:
+    'https://karigari-90b30.firebaseio.com/contact.json',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyB2n9wRI4Q3PzvySUNBiAu9w0ZpCHTWzwg',
+  firebaseJewelryDatabaseLink:
+    'https://karigari-90b30.firebaseio.com/jewelry.json',
+  firebaseContactDatabaseLink:
+    'https://karigari-90b30.firebaseio.com/contact.json',
 };
 
 /*
